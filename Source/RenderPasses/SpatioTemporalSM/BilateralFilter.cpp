b199ffa441dd7e234a2a3453c30e924b6a0f9d0e
AllocBlock <690051933@qq.com>
2022-01-14 12:17:57 +0800

    add compute shader version of bilateral filter (actually slower)

--- Source/RenderPasses/SpatioTemporalSM/BilateralFilter.cpp
+++ Source/RenderPasses/SpatioTemporalSM/BilateralFilter.cpp
@@ -1,4 +1,5 @@
 #include "BilateralFilter.h"
+#include "BilateralFilterConstants.slangh"
 
 namespace
 {
@@ -13,7 +14,8 @@ namespace
     const std::string kResult = "Result";
    
     // shader file path
-    const std::string kPassfile = "RenderPasses/SpatioTemporalSM/BilateralFilter.ps.slang";
+    const std::string kPixelPassfile = "RenderPasses/SpatioTemporalSM/BilateralFilter.ps.slang";
+    const std::string kComputePassfile = "RenderPasses/SpatioTemporalSM/BilateralFilter.cs.slang";
 }
 
 Gui::DropdownList STSM_BilateralFilter::mDirectionList =
@@ -21,6 +23,12 @@ Gui::DropdownList STSM_BilateralFilter::mDirectionList =
     Gui::DropdownValue{ int(EFilterDirection::X), toString(EFilterDirection::X) },
     Gui::DropdownValue{ int(EFilterDirection::Y), toString(EFilterDirection::Y) },
     Gui::DropdownValue{ int(EFilterDirection::BOTH), toString(EFilterDirection::BOTH) }
+};
+
+Gui::DropdownList STSM_BilateralFilter::mMethodList =
+{
+    Gui::DropdownValue{ int(EMethod::PIXEL_SHADER), toString(EMethod::PIXEL_SHADER) },
+    Gui::DropdownValue{ int(EMethod::COMPUTE_SHADER), toString(EMethod::COMPUTE_SHADER) },
 };
 
 STSM_BilateralFilter::STSM_BilateralFilter()
@@ -57,49 +65,39 @@ void STSM_BilateralFilter::compile(RenderContext* pContext, const CompileData& c
 
 void STSM_BilateralFilter::execute(RenderContext* pRenderContext, const RenderData& renderData)
 {
-    const auto& pColor = renderData[kColor]->asTexture();
-    const auto& pNormal = renderData[kNormal]->asTexture();
-    const auto& pDepth = renderData[kDepth]->asTexture();
-    const auto& pResult = renderData[kResult]->asTexture();
-
-    mVFilterPass.pPass["PerFrameCB"]["gEnable"] = mVContronls.Enable;
-    mVFilterPass.pPass["PerFrameCB"]["gSigmaColor"] = mVContronls.SigmaColor;
-    mVFilterPass.pPass["PerFrameCB"]["gSigmaNormal"] = mVContronls.SigmaNormal;
-    mVFilterPass.pPass["PerFrameCB"]["gSigmaDepth"] = mVContronls.SigmaDepth;
-    mVFilterPass.pPass["PerFrameCB"]["gKernelSize"] = mVContronls.KernelSize;
-    mVFilterPass.pPass["gTexNormal"] = pNormal;
-    mVFilterPass.pPass["gTexDepth"] = pDepth;
-
-    mVFilterPass.pPass["gTexColor"] = pColor;
-    mVFilterPass.pFbo->attachColorTarget(pResult, 0);
-    if (mVContronls.Direction != EFilterDirection::BOTH)
+    if (!mContronls.Enable)
     {
-        mVFilterPass.pPass["PerFrameCB"]["gDirection"] = (int)mVContronls.Direction;
-        mVFilterPass.pPass->execute(pRenderContext, mVFilterPass.pFbo);
+        const auto& pColor = renderData[kColor]->asTexture();
+        const auto& pResult = renderData[kResult]->asTexture();
+        pRenderContext->blit(pColor->getSRV(), pResult->getRTV());
+        return;
     }
-    else
+
+    switch (mContronls.Method)
     {
-        __prepareStageFbo(pResult);
-        mVFilterPass.pPass["PerFrameCB"]["gDirection"] = (int)EFilterDirection::X;
-        mVFilterPass.pPass->execute(pRenderContext, mVFilterPass.pStageFbo);
-        mVFilterPass.pPass["PerFrameCB"]["gDirection"] = (int)EFilterDirection::Y;
-        mVFilterPass.pPass["gTexColor"] = mVFilterPass.pStageFbo->getColorTexture(0);
-        mVFilterPass.pPass->execute(pRenderContext, mVFilterPass.pFbo);
+    case EMethod::PIXEL_SHADER: __executePixelPass(pRenderContext, renderData); break;
+    case EMethod::COMPUTE_SHADER: __executeComputePass(pRenderContext, renderData); break;
+    default: should_not_get_here();
     }
 }
 
 void STSM_BilateralFilter::renderUI(Gui::Widgets& widget)
 {
-    widget.checkbox("Enable", mVContronls.Enable);
-    if (mVContronls.Enable)
+    widget.checkbox("Enable", mContronls.Enable);
+    if (mContronls.Enable)
     {
-        uint Index = (uint)mVContronls.Direction;
-        widget.dropdown("Direction", mDirectionList, Index);
-        mVContronls.Direction = (EFilterDirection)Index;
-        widget.var("Sigma Color", mVContronls.SigmaColor, 1.0f, 50.0f, 0.1f);
-        widget.var("Sigma Normal", mVContronls.SigmaNormal, 1.0f, 50.0f, 0.1f);
-        widget.var("Sigma Depth", mVContronls.SigmaDepth, 1.0f, 50.0f, 0.1f);
-        widget.var("Kernel Size", mVContronls.KernelSize, 3u, 101u, 2u);
+        uint MethodIndex = (uint)mContronls.Method;
+        widget.dropdown("Method", mMethodList, MethodIndex);
+        mContronls.Method = (EMethod)MethodIndex;
+
+        uint DirectionIndex = (uint)mContronls.Direction;
+        widget.dropdown("Direction", mDirectionList, DirectionIndex);
+        mContronls.Direction = (EFilterDirection)DirectionIndex;
+
+        widget.var("Sigma Color", mContronls.SigmaColor, 1.0f, 50.0f, 0.1f);
+        widget.var("Sigma Normal", mContronls.SigmaNormal, 1.0f, 50.0f, 0.1f);
+        widget.var("Sigma Depth", mContronls.SigmaDepth, 1.0f, 50.0f, 0.1f);
+        widget.var("Kernel Size", mContronls.KernelSize, 3u, 101u, 2u);
     }
 }
 
@@ -114,16 +112,125 @@ std::string STSM_BilateralFilter::toString(EFilterDirection vType)
     }
 }
 
+std::string STSM_BilateralFilter::toString(EMethod vMethod)
+{
+    switch (vMethod)
+    {
+    case EMethod::PIXEL_SHADER: return "Pixel Shader";
+    case EMethod::COMPUTE_SHADER: return "Compute Shader";
+    default: should_not_get_here(); return "";
+    }
+}
+
 void STSM_BilateralFilter::__createPassResouces()
 {
-    mVFilterPass.pPass = FullScreenPass::create(kPassfile);
-    mVFilterPass.pFbo = Fbo::create();
+    // pixel pass
+    mPixelFilterPass.pPass = FullScreenPass::create(kPixelPassfile);
+    mPixelFilterPass.pFbo = Fbo::create();
+
+    // compute pass
+    ComputeProgram::SharedPtr pProgram = ComputeProgram::createFromFile(kComputePassfile, "main");
+    mComputeFilterPass.pVars = ComputeVars::create(pProgram->getReflector());
+    mComputeFilterPass.pState = ComputeState::create();
+    mComputeFilterPass.pState->setProgram(pProgram);
 }
 
 void STSM_BilateralFilter::__prepareStageFbo(Texture::SharedPtr vTarget)
 {
-    if (mVFilterPass.pStageFbo) return;
+    if (mPixelFilterPass.pStageFbo) return;
     Fbo::Desc fboDesc;
     fboDesc.setColorTarget(0, vTarget->getFormat());
-    mVFilterPass.pStageFbo = Fbo::create2D(vTarget->getWidth(), vTarget->getHeight(), fboDesc, vTarget->getArraySize());
+    mPixelFilterPass.pStageFbo = Fbo::create2D(vTarget->getWidth(), vTarget->getHeight(), fboDesc, vTarget->getArraySize());
+}
+
+void STSM_BilateralFilter::__prepareStageTexture(Texture::SharedPtr vTarget)
+{
+    _ASSERTE(mContronls.Method == EMethod::COMPUTE_SHADER);
+    if (mContronls.Direction == EFilterDirection::BOTH && mComputeFilterPass.pStageTexture[0] && mComputeFilterPass.pStageTexture[1]) return;
+    if (mContronls.Direction != EFilterDirection::BOTH && mComputeFilterPass.pStageTexture[0]) return;
+
+    for (int i = 0; i < 2; ++i)
+    {
+        if (mComputeFilterPass.pStageTexture[i]) continue; // empty
+        else if (i == 1 && mContronls.Direction != EFilterDirection::BOTH) continue; // 2nd texture is not necessary
+        mComputeFilterPass.pStageTexture[i] = Texture::create2D(vTarget->getWidth(), vTarget->getHeight(), vTarget->getFormat(), vTarget->getArraySize(), 1, nullptr, ResourceBindFlags::ShaderResource | ResourceBindFlags::UnorderedAccess);
+    }
+}
+
+void STSM_BilateralFilter::__executePixelPass(RenderContext* vRenderContext, const RenderData& vRenderData)
+{
+    const auto& pColor = vRenderData[kColor]->asTexture();
+    const auto& pNormal = vRenderData[kNormal]->asTexture();
+    const auto& pDepth = vRenderData[kDepth]->asTexture();
+    const auto& pResult = vRenderData[kResult]->asTexture();
+
+    mPixelFilterPass.pPass["PerFrameCB"]["gSigmaColor"] = mContronls.SigmaColor;
+    mPixelFilterPass.pPass["PerFrameCB"]["gSigmaNormal"] = mContronls.SigmaNormal;
+    mPixelFilterPass.pPass["PerFrameCB"]["gSigmaDepth"] = mContronls.SigmaDepth;
+    mPixelFilterPass.pPass["PerFrameCB"]["gKernelSize"] = mContronls.KernelSize;
+    mPixelFilterPass.pPass["gTexNormal"] = pNormal;
+    mPixelFilterPass.pPass["gTexDepth"] = pDepth;
+
+    mPixelFilterPass.pPass["gTexColor"] = pColor;
+    mPixelFilterPass.pFbo->attachColorTarget(pResult, 0);
+    if (mContronls.Direction != EFilterDirection::BOTH)
+    {
+        mPixelFilterPass.pPass["PerFrameCB"]["gDirection"] = (int)mContronls.Direction;
+        mPixelFilterPass.pPass->execute(vRenderContext, mPixelFilterPass.pFbo);
+    }
+    else
+    {
+        __prepareStageFbo(pResult);
+        mPixelFilterPass.pPass["PerFrameCB"]["gDirection"] = (int)EFilterDirection::X;
+        mPixelFilterPass.pPass->execute(vRenderContext, mPixelFilterPass.pStageFbo);
+        mPixelFilterPass.pPass["PerFrameCB"]["gDirection"] = (int)EFilterDirection::Y;
+        mPixelFilterPass.pPass["gTexColor"] = mPixelFilterPass.pStageFbo->getColorTexture(0);
+        mPixelFilterPass.pPass->execute(vRenderContext, mPixelFilterPass.pFbo);
+    }
+}
+
+void STSM_BilateralFilter::__executeComputePass(RenderContext* vRenderContext, const RenderData& vRenderData)
+{
+    const auto& pColor = vRenderData[kColor]->asTexture();
+    const auto& pNormal = vRenderData[kNormal]->asTexture();
+    const auto& pDepth = vRenderData[kDepth]->asTexture();
+    const auto& pResult = vRenderData[kResult]->asTexture();
+
+    // calculate compute shader param
+    uint TexWidth = pResult->getWidth();
+    uint TexHeight = pResult->getHeight();
+
+    uint32_t NumGroupX = div_round_up((int)TexWidth, _BILATERAL_THREAD_NUM_X);
+    uint32_t NumGroupY = div_round_up((int)TexHeight, _BILATERAL_THREAD_NUM_Y);
+    uint3 DispatchDim = uint3(NumGroupX, NumGroupY, 1u);
+
+    __prepareStageTexture(pResult);
+
+    mComputeFilterPass.pVars["PerFrameCB"]["gSigmaColor"] = mContronls.SigmaColor;
+    mComputeFilterPass.pVars["PerFrameCB"]["gSigmaNormal"] = mContronls.SigmaNormal;
+    mComputeFilterPass.pVars["PerFrameCB"]["gSigmaDepth"] = mContronls.SigmaDepth;
+    mComputeFilterPass.pVars["PerFrameCB"]["gKernelSize"] = mContronls.KernelSize;
+    mComputeFilterPass.pVars["gTexNormal"] = pNormal;
+    mComputeFilterPass.pVars["gTexDepth"] = pDepth;
+
+    mComputeFilterPass.pVars["gTexColor"] = pColor;
+    mComputeFilterPass.pVars["gTexOutput"] = mComputeFilterPass.pStageTexture[0];
+
+    int SrcIndex = 0;
+    if (mContronls.Direction != EFilterDirection::BOTH)
+    {
+        mComputeFilterPass.pVars["PerFrameCB"]["gDirection"] = (int)mContronls.Direction;
+        vRenderContext->dispatch(mComputeFilterPass.pState.get(), mComputeFilterPass.pVars.get(), DispatchDim);
+    }
+    else
+    {
+        SrcIndex = 1;
+        mComputeFilterPass.pVars["PerFrameCB"]["gDirection"] = (int)EFilterDirection::X;
+        vRenderContext->dispatch(mComputeFilterPass.pState.get(), mComputeFilterPass.pVars.get(), DispatchDim);
+        mComputeFilterPass.pVars["PerFrameCB"]["gDirection"] = (int)EFilterDirection::Y;
+        mComputeFilterPass.pVars["gTexColor"] = mComputeFilterPass.pStageTexture[0];
+        mComputeFilterPass.pVars["gTexOutput"] = mComputeFilterPass.pStageTexture[1];
+        vRenderContext->dispatch(mComputeFilterPass.pState.get(), mComputeFilterPass.pVars.get(), DispatchDim);
+    }
+    vRenderContext->blit(mComputeFilterPass.pStageTexture[SrcIndex]->getSRV(), pResult->getRTV());
 }
--- Source/RenderPasses/SpatioTemporalSM/BilateralFilter.h
+++ Source/RenderPasses/SpatioTemporalSM/BilateralFilter.h
@@ -32,31 +32,50 @@ public:
         BOTH
     };
 
+    enum class EMethod : uint
+    {
+        PIXEL_SHADER,
+        COMPUTE_SHADER
+    };
+
     static std::string toString(EFilterDirection vType);
+    static std::string toString(EMethod vMethod);
 
 private:
     STSM_BilateralFilter();
 
     static Gui::DropdownList mDirectionList;
+    static Gui::DropdownList mMethodList;
 
     struct  
     {
         FullScreenPass::SharedPtr pPass;
         Fbo::SharedPtr pFbo;
         Fbo::SharedPtr pStageFbo;
-    } mVFilterPass;
+    } mPixelFilterPass;
+
+    struct
+    {
+        ComputeState::SharedPtr pState;
+        ComputeVars::SharedPtr pVars;
+        Texture::SharedPtr pStageTexture[2];
+    } mComputeFilterPass;
 
     struct 
     {
         bool Enable = true;
+        EMethod Method = EMethod::PIXEL_SHADER;
         float SigmaColor = 10.0f;
         float SigmaNormal = 10.0f;
         float SigmaDepth = 10.0f;
         uint KernelSize = 15u;
         EFilterDirection Direction = EFilterDirection::BOTH;
-    } mVContronls;
+    } mContronls;
 
 
     void __createPassResouces();
     void __prepareStageFbo(Texture::SharedPtr vTarget);
+    void __prepareStageTexture(Texture::SharedPtr vTarget);
+    void __executePixelPass(RenderContext* vRenderContext, const RenderData& vRenderData);
+    void __executeComputePass(RenderContext* vRenderContext, const RenderData& vRenderData);
 };

c56a0dcf7c3697abe387f6b79b3b825b127b3511
krNeko9t <42951801+krNeko9t@users.noreply.github.com>
2022-03-02 21:17:32 +0800

    G-O exp

--- Data/Script/Ghosting.py
+++ Data/Script/Ghosting.py
@@ -1,16 +1,17 @@
 from xml.dom.expatbuilder import ExpatBuilderNS
 from falcor import *
 
-GraphPath = '../../Data/Graph/'
-ScenePath = '../../../Data/Scene/'
-SceneSubPath = 'Experiment/Ghosting/'
-
 ExpMainName = 'Ghosting'
 ExpSubName = 'Object'
 ExpAlgorithmName = ['SRGM','TA','GroundTruth']
 ExpIdx = 0
 
-GraphName = 'GraphSTSM_MS_FINAL.py'
+GraphPath = '../../Data/Graph/'
+ScenePath = '../../../Data/Scene/'
+SceneSubPath = 'Experiment/' + ExpMainName + '/'
+
+# GraphName = 'GraphSTSM_MS_FINAL.py'
+GraphName = 'Ghosting-Object.py'
 SceneName = ExpSubName+'.pyscene'
 
 OutputPath = "d:/Out/Ghosting/" + ExpSubName
@@ -22,19 +23,19 @@ FramesToCapture = range(200,210)
 m.script(GraphPath+GraphName)
 m.loadScene(ScenePath+SceneSubPath+SceneName)
 
-m.clock.framerate = 30
+m.clock.framerate = 120
 
-# m.clock.stop()
-# m.frameCapture.outputDir = OutputPath
+m.clock.stop()
+m.frameCapture.outputDir = OutputPath
 
-# for i in range(TotalFrame):
-#     renderFrame()
-#     if i in FramesToCapture:
-#         # just for ground truth
-#         if ExpIdx == 2 :
-#             for j in range(0,100):
-#                 renderFrame()
-#         m.frameCapture.baseFilename = ExpName + f"-{i:04d}"
-#         m.frameCapture.capture()
-#     m.clock.step()
-# exit()
+for i in range(TotalFrame):
+    renderFrame()
+    if i in FramesToCapture:
+        # just for ground truth
+        if ExpIdx == 2 :
+            for j in range(0,100):
+                renderFrame()
+        m.frameCapture.baseFilename = ExpName + f"-{i:04d}"
+        m.frameCapture.capture()
+    m.clock.step()
+exit()

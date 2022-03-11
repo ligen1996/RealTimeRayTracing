88e3320a33f96bf0c1458720da41afb5b9a829db
AllocBlock <37104958+AllocBlock@users.noreply.github.com>
2022-03-11 14:30:39 +0800

    add dynamic observe for convergence exp

--- Data/Script/plotConvergence.py
+++ Data/Script/plotConvergence.py
@@ -38,7 +38,7 @@ def findSameFrame(vDir, vFrameId):
             return FileName
     return None
 
-def run(vBaseDir, vDirGT, vDirTarget):
+def calConvergence(vBaseDir, vDirGT, vDirTarget):
     CompareResult = []
     GTImages = os.listdir(vBaseDir + vDirGT)
 
@@ -74,6 +74,43 @@ def run(vBaseDir, vDirGT, vDirTarget):
             TargetResults[TargetName]['SSIM'].append(SSIM)
     return [TargetResults, Num]
 
+def calFlicking(vBaseDir, vDirGT, vDirTarget):
+    CompareResult = []
+    GTImages = os.listdir(vBaseDir + vDirGT)
+
+    Num = len(GTImages)
+    TargetResults = {}
+    for Target in vDirTarget:
+        TargetName = Target['Name']
+        TargetResults[TargetName] = {}
+        TargetResults[TargetName]['RMSE'] = []
+        TargetResults[TargetName]['SSIM'] = []
+
+    print("Calculating measure...")
+    for i in range(Num - 1):
+        FrameId = extractFrameId(GTImages[i])
+        if FrameId == None:
+            print("Can parse file name ", GTImage)
+            continue
+        
+        print("Current frame id:", FrameId)
+
+        for Target in vDirTarget:
+            TargetName = Target['Name']
+            TargetImageCur = findSameFrame(vBaseDir + Target['Dir'], FrameId)
+            TargetImageNext = findSameFrame(vBaseDir + Target['Dir'], FrameId + 1)
+            if not TargetImageCur or not TargetImageNext:
+                print("No match found for file ", GTImage)
+                continue
+            RMSE = getRMSE(vBaseDir + Target['Dir'] + TargetImageCur, vBaseDir + Target['Dir'] + TargetImageNext)
+            SSIM = getSSIM(vBaseDir + Target['Dir'] + TargetImageCur, vBaseDir + Target['Dir'] + TargetImageNext)
+            print("  ", TargetImageCur, " - ", TargetImageNext)
+            print("  RMSE: ", RMSE, " SSIM", SSIM)
+
+            TargetResults[TargetName]['RMSE'].append(RMSE)
+            TargetResults[TargetName]['SSIM'].append(SSIM)
+    return [TargetResults, Num - 1]
+
 def plot(vResult, vNum, vTitle):
     fig = plt.figure()
     plt.title(vTitle, fontsize = 24)
@@ -105,31 +142,32 @@ DirGT = "GroundTruth/AccumulatePass-output/"
 DirTarget = [
     {
         'Name': 'Random',
-        'Dir': "Random/STSM_TemporalReuse-TR_Visibility/"
+        'Dir': "Random/STSM_BilateralFilter-Result/"
     },
     {
         'Name': 'Tranditional_16',
-        'Dir': "Tranditional_16/STSM_TemporalReuse-TR_Visibility/"
+        'Dir': "Tranditional_16/STSM_BilateralFilter-Result/"
     },
 ]
 
-def getOutputFile(Scene = 'Grid', Type = 'Object'):
-    return BaseDir + "plotData_%s_%s.json" %(Scene, Type)
+def getOutputFile(Scene):
+    return BaseDir + "plotData_%s_%s.json" % (gCalTypes[gCalTypeIdx], Scene)
 
-gReadFromFile = False
-for Scene in ['Grid', 'Dragon', 'Arcade']:
-    for Type in ['Object', 'Light']:
-        print("Run plot for ", Scene, Type)
-        SubDir = Scene + "/" + Type + "/"
-        OutputFile = getOutputFile(Scene, Type)
-        if gReadFromFile:
-            [Result, Num] = readJson(OutputFile)
+gCalTypes = ["Covergence", "Flicking"]
+gCalTypeIdx = 1
+gReadFromFile = True
+# for Scene in ['GridObserve', 'DragonObserve', 'ArcadeObserve']:
+for Scene in ['DynamicGridObserve', 'DynamicDragonObserve', 'DynamicArcadeObserve']: # dynamic
+    print("Run plot for ", Scene)
+    SubDir = Scene + "/"
+    OutputFile = getOutputFile(Scene)
+    if gReadFromFile:
+        [Result, Num] = readJson(OutputFile)
+    else:
+        if (gCalTypeIdx == 0):
+            [Result, Num] = calConvergence(BaseDir + SubDir, DirGT, DirTarget)
         else:
-            [Result, Num] = run(BaseDir + SubDir, DirGT, DirTarget)
-            writeJson(OutputFile, [Result, Num])
-        print("Ploting...")
-        plot(Result, Num, "%s - %s" % (Scene, Type))
-        
-
-
-os.system("pause")
+            [Result, Num] = calFlicking(BaseDir + SubDir, DirGT, DirTarget)
+        writeJson(OutputFile, [Result, Num])
+    print("Ploting...")
+    plot(Result, Num, Scene + "-" + gCalTypes[gCalTypeIdx])
